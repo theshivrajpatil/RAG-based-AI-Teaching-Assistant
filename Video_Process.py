05496f84390dfb7427a1969699a3a604dd009c0d
theshivrajpatil <theshivrajpatil@gmail.com>
2026-02-10 20:47:42 +0530

    saving audio file name

--- Video_Process.py
+++ Video_Process.py
@@ -1,1 +1,11 @@
-import whisper
+# Converts the videos to mp3 
+import os 
+import subprocess
+
+files = os.listdir("videos") 
+for file in files: 
+    
+    tutorial_number = file.split(".")[0].split(" -")[0]
+    file_name = file.split("｜")[0].split(" -")[1]
+    print(tutorial_number, file_name)
+    
