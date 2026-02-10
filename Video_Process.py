8b9fc81a56d6bb6a365301d19087f045846cdf0f
theshivrajpatil <theshivrajpatil@gmail.com>
2026-02-10 21:07:30 +0530

    Using subprocess for converting mp4 into mp3

--- Video_Process.py
+++ Video_Process.py
@@ -4,8 +4,8 @@ import subprocess
 
 files = os.listdir("videos") 
 for file in files: 
-    
     tutorial_number = file.split(".")[0].split(" -")[0]
     file_name = file.split("｜")[0].split(" -")[1]
     print(tutorial_number, file_name)
+    subprocess.run(["ffmpeg", "-i", f"videos/{file}", f"audios/{tutorial_number}_{file_name}.mp3"])
     
