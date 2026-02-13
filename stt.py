7131b2344db47bbf8237fb9b6a66412fd86341d2
theshivrajpatil <theshivrajpatil@gmail.com>
2026-02-13 13:57:36 +0530

    modify

--- stt.py
+++ stt.py
@@ -2,5 +2,5 @@ import whisper
 
 model = whisper.load_model("large-v2")
 
-# result = model.transcribe("audios/1_ Python Full Course❤️ .mp3")
+result = model.transcribe("audios/1_ Python Full Course❤️ .mp3")
 # print(result["text"])
