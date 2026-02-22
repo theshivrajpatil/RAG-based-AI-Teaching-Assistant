fc3f00a1a8fd05db59afb886025c889ab56327e4
theshivrajpatil <theshivrajpatil@gmail.com>
2026-02-22 13:05:51 +0530

    cosine_similarity

--- read_chunk.py
+++ read_chunk.py
@@ -37,15 +37,15 @@ for json_file in jsons:
 df = pd.DataFrame.from_records(my_dicts) 
 incoming_query = input("Ask a Question: ")
 question_embedding = create_embedding([incoming_query])[0] 
-print(question_embedding)
+# print(question_embedding)
 
 # Find similarities of question_embedding with other embeddings
 # print(np.vstack(df['embedding'].values))
 # print(np.vstack(df['embedding']).shape)
-# similarities = cosine_similarity(np.vstack(df['embedding']), [question_embedding]).flatten()
-# print(similarities)
+similarities = cosine_similarity(np.vstack(df['embedding']), [question_embedding]).flatten()
+print(similarities)
 # top_results = 3
 # max_indx = similarities.argsort()[::-1][0:top_results]
 # print(max_indx)
 # new_df = df.loc[max_indx] 
-# print(new_df[["title", "number", "text"]])
+# print(new_df[["title", "number", "text"]]) 
