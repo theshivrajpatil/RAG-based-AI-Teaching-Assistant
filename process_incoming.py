5c56d7c6b69851f56ede7648accb3b2ff608e4fe
theshivrajpatil <theshivrajpatil@gmail.com>
2026-02-24 19:40:33 +0530

    used joblib

---
+++ process_incoming.py
@@ -0,0 +1,32 @@
+import requests
+import json
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+import pandas as pd
+import joblib
+
+
+def create_embedding(text_list):
+    # https://github.com/ollama/ollama/blob/main/docs/api.md#generate-embeddings
+    r = requests.post("http://localhost:11434/api/embed", json={
+        "model": "bge-m3",
+        "input": text_list
+    })
+
+    embedding = r.json()["embeddings"] 
+    return embedding
+
+# Load the dataframe
+df = joblib.load("embeddings.joblib")
+
+incoming_query = input("Ask a Question: ")
+question_embedding = create_embedding([incoming_query])[0]
+
+# Find similarities of question_embedding with other embeddings
+similarities = cosine_similarity(np.vstack(df['embedding']), [question_embedding]).flatten()
+print(similarities)
+top_results = 3
+max_indx = similarities.argsort()[::-1][0:top_results]
+print(max_indx)
+new_df = df.loc[max_indx]
+print(new_df[["title", "number", "text"]])
